e3243e6c1ffb33963e9523fa25736b457be918ea
Jair <jairaceves56@gmail.com>
2024-07-12 11:25:34 -0600

    Landing CCAmigos

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import { Formulario } from "./Formulario"
+
+export const Banner = () => {
+  return (
+    <>
+        <div className="flex justify-center">
+            <img src="https://ccamigos.com.mx/congreso-musicos/assets/img/banner-principal.jpeg" className="max-w-full h-auto rounded-lg"></img>
+        </div>
+    </>
+  )
+}
